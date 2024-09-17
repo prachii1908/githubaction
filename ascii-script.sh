aeccab9c956cbed54e76b2134ad74756840c81cc
prachii1908 <74233961+prachii1908@users.noreply.github.com>
2024-09-17 15:09:41 +0530

    updated shell script

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 sudo apt-get install cowsay -y
-cowsay -f dog "Run for cover, I am a DOG...BARK" >> dog.txt
-grep -i "dog" dog.txt
-cat dog.txt
+cowsay -f cow "Run for cover, I am a COW...AHMMBHEE" >> cow.txt
+grep -i "cow" cow.txt
+cat cow.txt
 ls -ltra
